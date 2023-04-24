22836d86d3a530424be629e9b08018c2c1fc3c5f
zuru-zuru <jaivardhans46@gmail.com>
2023-04-24 16:10:25 -0400

    hi

--- translate.py
+++ translate.py
@@ -1,10 +1,12 @@
 from google.cloud import translate_v2 as translate
 
 # Initialize the client
-translate_client = translate.Client.from_service_account_json('credents.json')
-text = 'Hello, how are you?'
-source_language = 'en'
-target_language = 'es'
-translation = translate_client.translate(text, source_language=source_language, target_language=target_language)
-print(translation['input'])
-print(translation['translatedText'])
+def translate_review(rev, target_language = "en"):
+    translate_client = translate.Client.from_service_account_json('credents.json')
+    text = rev
+    result = translate_client.detect_language(text)
+    source_lang = result["language"]
+    translation = translate_client.translate(text, source_language=source_lang, target_language=target_language)
+    return (translation['translatedText'])
+
+print(translate_review("Bonjour?"))
